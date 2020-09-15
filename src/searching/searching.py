b8a2f452cf5ec7b89ea5a35f089d62c382666742
Anthony N <foto.arn@gmail.com>
2020-09-14 20:45:45 -0700

    + binary_search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -13,6 +13,15 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-
-
+    low = 0
+    high = len(arr) - 1
+    middle = 0
+    while low <= high:
+        middle = (low + high) // 2
+        if target < arr[middle]:
+            high = middle - 1  # Remove right
+        elif target > arr[middle]:
+            low = middle + 1  # Remove left
+        else:
+            return middle
     return -1  # not found
